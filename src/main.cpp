5c0a94c15545b8db0e5376bba8143168eec7be01
HeyFang <heyfang36@yahoo.com>
2026-09-26 00:28:20 +0530

    scannin songs n playlist

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <filesystem>
+
 
 int main(){
-  std::cout << "jus-music";
+
+  std::filesystem::path music="music";
+
+  for(const auto& entry : std::filesystem::directory_iterator(music)){
+    if(entry.is_directory()){
+      std::cout << "[Playlist]" << entry.path() << '\n';
+    }
+    else if(entry.is_regular_file()){
+      std::cout << "[Song]" << entry.path() << '\n';
+    }
+  }
 
   return 0;
 }
