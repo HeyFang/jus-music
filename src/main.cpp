a53642afd36657c7b69a1219babd556053a80843
HeyFang <heyfang36@yahoo.com>
2026-09-27 00:08:19 +0530

    added supported file extensions

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,13 @@ int main(){
     if(entry.is_directory()){
       std::cout << "[Playlist]" << entry.path() << '\n';
     }
-    else if(entry.is_regular_file()){
+    else if(entry.is_regular_file() && (entry.path().extension() == ".mp3" || 
+            entry.path().extension() == ".wav" ||
+            entry.path().extension() == ".flac" ||
+            entry.path().extension() == ".m4a" ||
+            entry.path().extension() == ".aac" ||
+            entry.path().extension() == ".ogg")
+          ){
       std::cout << "[Song]" << entry.path() << '\n';
     }
   }
